901f0072572833439dd4e42184f85153db04d024
Sagar Patil <sagarkp3@gmail.com>
2018-09-07 11:03:20 +0530

    added push patch delete

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,21 +1,51 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
-from . import models
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from . import serializers
+from rest_framework import status
+
+
 # Create your views here.
 
-
 class HelloApiView(APIView):
+    """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
         an_apiview = [
-            'Uses HTTP methods as functions (get, post, patch, put, delete)',
-            'Is similar to a traditional Django View',
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'It is similar to a traditional Django view',
             'Gives you the most control over your logic',
-            'Is mapped manually to URLs',
+            'Is mapped manually to URLs'
         ]
 
-        return Response({'message':'message','api_view': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+
+        return Response({'method':'patch'})
+
+    def delete(self,request, pk=None):
+        return Response({'method':'delete'})
