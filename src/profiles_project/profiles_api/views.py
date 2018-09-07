2d949053f820e54f53b6bbc56f46ee81f46b6021
Sagar Patil <sagarkp3@gmail.com>
2018-09-07 15:01:26 +0530

    added filters for search

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,7 @@ from . import models
 from . import permissions
 from rest_framework.authentication import TokenAuthentication
 from . import serializers
+from rest_framework import filters
 
 
 # Create your views here.
@@ -109,3 +110,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
